8610f6b86f405117c2224fcb59b59088cfbe31c0
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-07-03 16:05:54 +0700

    Edit: Error Message Position

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,17 +10,17 @@
     <header>
         Total Usage : {{ App\Models\History::all()->count() }}
     </header>
-    @error('video_url')
-    <div class="alert alert-error">
-        {{ $message }}
-    </div>
-    @enderror
     <section class="hero @if (!isset($thumbnails)) h-100 @endif">
         <h1><span>Youtube</span> Thumbnail<br>Downloader</h1>
         <form class="search-bar" method="GET" action="{{ route('youtube.thumbnail') }}">
             <input type="text" name="video_url" placeholder="Paste Youtube Video URL here...">
             <button class="btn">Search</button>
         </form>
+        @error('video_url')
+        <div class="alert alert-error">
+            {{ $message }}
+        </div>
+        @enderror
     </section>
 
     @if (isset($thumbnails))
